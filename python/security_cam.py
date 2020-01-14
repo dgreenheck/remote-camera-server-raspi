94b83a4ee72de392a064e6ac03582ab79d53fd23
Admin <dgreenheck@gmail.com>
2020-01-14 00:06:22 -0800

    Added ability to specify custom framerates. Moved .mp4 encoding to separate thread. Suppressed console output.

--- python/security_cam.py
+++ python/security_cam.py
@@ -2,6 +2,7 @@ import picamera
 from datetime import datetime
 import os
 import subprocess
+import threading
 
 def encode_h264_to_mp4(framerate,input,output):
   # encode_mp4 - Encodes .h264 video to .mp4
@@ -11,17 +12,20 @@ def encode_h264_to_mp4(framerate,input,output):
   #  - input: Input .h264 video path
   #  - output: Output .mp4 video path
   print('Encoding .h264 to .mp4 ({input} -> {output})'.format(input=input, output=output))
-  command = 'ffmpeg -i "{input}" -c:v copy  -c:a copy -movflags +faststart "{output}"'.format(input=input, output=output)
+  command = 'ffmpeg -loglevel quiet -framerate {framerate} -i "{input}" -c:v copy -c:a copy -movflags +faststart "{output}"'.format(framerate=framerate, input=input, output=output)
   print(command)
+  # Convert the video
   subprocess.call(command, shell=True)
+  # Delete the raw input file
+  os.remove(input)
 
 # Length in seconds of each video file
 FILE_TIME_LIMIT = 10
 # Framerate (frames per second)
-FRAMERATE = 30
+FRAMERATE = 5
 
 # Directory to store recordings
-directory = '/home/admin/recordings/'
+directory = '//home/admin/recordings/'
 
 # Loop the video capture indefinitely
 while 1:
@@ -35,15 +39,14 @@ while 1:
   camera.framerate = FRAMERATE
   try:
     camera.start_recording(raw_path,format='h264')
-    print('Camera recording ' + filename + '...')
+    print('Starting camera recording ' + filename + '...')
     camera.wait_recording(FILE_TIME_LIMIT)
     camera.stop_recording()
-    print('Recording finished')
   finally:
     camera.close()
-    print('Closing camera')
 
-  # Convert the video file to .mp4
+  # Convert the video file to .mp4 in a separate thread
   encoded_path = directory + filename + '.mp4'
-  encode_h264_to_mp4(FRAMERATE,raw_path,encoded_path)
- 
+  thread = threading.Thread(target=encode_h264_to_mp4, args=(FRAMERATE,raw_path,encoded_path))
+  thread.daemon = True
+  thread.start() 
